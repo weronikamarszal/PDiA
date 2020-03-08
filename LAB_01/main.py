054ac81dbb7aff188677a7c75a8a510382d0d2f2
weromarszal <wero.marszal@gmail.com>
2020-03-08 21:57:01 +0100

    init lab01

---
+++ LAB_01/main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import numpy as np
+import random
+
+# a = np.array([1, 2, 3, 4, 5, 6, 7]);
+# print(a);
+
+# b=np.array([[1,2,3,4,5], [6,7,8,9,10]]);
+# print(b);
+
+# c=np.transpose(b);
+# print(c);
+
+# d=np.arange(100);
+# print(d);
+
+# e=np.linspace(1,2,10);
+# print(e);
+
+# f=np.arange(0,101,5);
+# print(f);
+
+# g=np.random.rand(20).round(2);
+# print(g);
+
+# h=np.random.randint(1,1000,100);
+# print(h);
+
+# i=np.zeros((3,2));
+# print(i);
+
+# j=np.ones((3,2));
+# print(j);
+
+# k=np.random.randint(10000,size=(5,5));
+# k=np.float32(k);
+# print(k);
