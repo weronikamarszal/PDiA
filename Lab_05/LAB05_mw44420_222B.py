a40483d48ce64f83982d2cd4aa21ac641b33afc4
weronikamarszal <m.weero@gmail.com>
2020-04-18 13:35:32 +0200

    lab5 zad3

--- Lab_05/LAB05_mw44420_222B.py
+++ Lab_05/LAB05_mw44420_222B.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import scipy.sparse
-
-data = pd.read_csv('zoo.csv');
+import math
 
 
 def unique(x):
@@ -24,26 +23,129 @@ def freq(x, prob):
 
     if (prob == True):
         for i in range(len(xi)):
-            p.append(ni.array[i]/len(xi))
+            p.append(ni.array[i] / len(xi))
         return xi, p
 
     return xi, ni;
 
-def freq2(x, y, p):
+
+def freq2(x, y, prob):
     xi = unique(x);
     yi = unique(y)
-    x1 = x + y;
-    u, ni = freq(x1, prob=False);
-    if (p == True):
-        u1, p = freq(x1, prob=True);
-        return xi, yi, p;
-    return xi, yi, ni;
+    xyi = []
+    nx = len(xi);
+    ny = len(yi);
 
 
+    for i in range(nx):
+        for j in range(ny):
+            xyi.append([xi[i], yi[j]])
+
+    tmp = 0
+    nxy = len(xyi)
+    ni = []
+    for i in range(nxy):
+        for j in range(nx):
+            if [x[j], y[j]] == xyi[i]:
+                tmp +=  1
+        ni.append(tmp)
+        tmp = 0
+
+    if prob == True:
+        pi = []
+        for i in range(len(ni)):
+            pi.append(ni[i] / len(x))
+        return xi, yi, xyi, pi
+
+    return xi, yi, xyi, ni
+
+
+
+
+def entropy(x):
+    xi, pi = freq(x, prob=True);
+    n = len(pi);
+    sum = 0;
+    for i in range(n):
+        sum += pi[i] * math.log2(pi[i]);
+
+    ent = -sum;
+    return ent;
+
+
+def entropyxy(x, y):
+    xi, yi, xyi, pi = freq2(x, y, prob=True);
+    n = len(xyi);
+    sum = 0;
+
+
+    for i in range(n):
+        if pi[i] != 0:
+            sum += pi[i] * math.log2(pi[i])
+
+    ent = -sum
+    return ent;
+
+def funkcjaPomocnicza(x, y):
+    xi, yi, xyi, pi = freq2(x, y, prob=True);
+    sum=0;
+
+    for i in range(len(pi)):
+        if pi[i] != 0:
+            sum += pi[i] * math.log2(pi[i]);
+
+    hYxi = -sum;
+    return hYxi;
+
+def entropiaWarunkowa(x, y):
+    xi, yi, xyi, pi = freq2(x, y, prob=True);
+    x1, px = freq(x, prob=True);
+    ent = 0;
+
+    for j in range(len(px)):
+        ent += px[j] * funkcjaPomocnicza(x,y);
+
+    return ent
+
+
+def informacjaWzajemna(x, y):
+    hx = entropy(x);
+    hy = entropy(y);
+    hxy = entropyxy(x, y)
+    inf = hx + hy - hxy;
+    return inf;
+
+def infogain(x,y):
+    hy=entropy(y);
+    entCond = entropiaWarunkowa(x, y);
+    inf = hy - entCond;
+    return inf;
+
+
+######################
 x = [1, 1, 1, 2, 3, 4]
 y = [5, 5, 2, 4, 5, 9]
 # zad1
 # print(freq(x, prob = True))
 
 # zad2
-print(freq2(x, y, p=False))
+print(freq2(x, y, prob=True))
+
+# ZAD3
+hx = entropy(x);
+hy = entropy(y);
+hxy = entropyxy(x, y);
+# print(hxy);
+# inf = informacjaWzajemna(x, y);
+# print(inf);
+print(entropiaWarunkowa(x,y));
+print(infogain(x,y));
+#
+# # ZAD4
+data = pd.read_csv('zoo.csv');
+
+
+
+
+from sklearn.datasets import fetch_rcv1
+
